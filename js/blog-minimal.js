caa594004d90c1d108bc7cd02c150fc70ee2fe6d
nagygyorgergo <nagygyor.gergo@gmail.com>
2023-09-20 18:25:49 +0200

    API kepek es leirasok betoltese az oldal also reszen, pagination elore-hatra gombokkal

--- js/blog-minimal.js
+++ js/blog-minimal.js
@@ -1,3 +1,4 @@
+//Functions for navbar dropdown menu
 function inspirationShow() {
     const inspirationDropdown = document.getElementById("inspirationDropdown");
     inspirationDropdown.classList.toggle("show");
@@ -15,8 +16,52 @@ function closeCoursesDropdown() {
     coursesDropdown.classList.remove("show");
 }
 
-// Data fetching from API
-const fetchData = async (url, id) => {
+// Data fetching from API for the slideshow
+const fetchSlideshowData = async (url, id) => {
+    try {
+        const response = await fetch(url + id.toString());
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        // Extract the URL and title of the photo from the JSON response
+        const photoUrl = data.photo.url;
+        const photoTitle = data.photo.title;
+
+        // Get the img element by its ID and the title by the title attribute
+        const photoElement = document.getElementById('photo' + (id % 3).toString());
+        const titleElement = document.getElementById('title' + (id % 3).toString());
+
+        // Set the src attribute of the img element to the photo URL and the content of the title
+        photoElement.src = photoUrl;
+        titleElement.textContent = photoTitle;
+        //console.log('Photo URL:', photoUrl);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+const URL = 'https://api.slingacademy.com/v1/sample-data/photos/';
+let SlideNumber = 4; // Initialize to starter image
+fetchSlideshowData(URL, SlideNumber);
+
+document.getElementById('forwardButton').addEventListener('click', () => {
+    if(SlideNumber<6){
+        SlideNumber++;
+        fetchSlideshowData(URL, SlideNumber);
+    }
+});
+
+document.getElementById('backButton').addEventListener('click', () => {
+    if(SlideNumber>4){
+        SlideNumber--;
+        fetchSlideshowData(URL, SlideNumber);
+    }
+});
+
+//Fetching data for other images
+const fetchImageData = async (url, id) => {
     try {
         const response = await fetch(url + id.toString());
         if (!response.ok) {
@@ -27,34 +72,50 @@ const fetchData = async (url, id) => {
         // Extract the URL of the photo from the JSON response
         const photoUrl = data.photo.url;
         const photoTitle = data.photo.title;
+        const photoDescription = data.photo.description;
+        const userName = data.photo.user;
 
         // Get the img element by its ID
-        const photoElement = document.getElementById('photo' + (id % 3).toString());
-        const titleElement = document.getElementById('title' + (id % 3).toString());
+        const photoElement = document.getElementById('listImage' + (id % 5).toString());
+        const titleElement = document.getElementById('listTitle' + (id % 5).toString());
+        const descriptionElement = document.getElementById('description' + (id % 5).toString());
+        const usernameElement = document.getElementById('username' + (id % 5).toString());
         // Set the src attribute of the img element to the photo URL
         photoElement.src = photoUrl;
         titleElement.textContent = photoTitle;
+        descriptionElement.textContent = photoDescription;
+        usernameElement.textContent = "By: "+userName;
         console.log('Photo URL:', photoUrl);
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-const URL = 'https://api.slingacademy.com/v1/sample-data/photos/';
-let szam = 4; // Initialize to 0
-fetchData(URL, szam);
-// Function to load the next set of images
-
-document.getElementById('eloreGomb').addEventListener('click', () => {
-    if(szam<6){
-        szam++;
-        fetchData(URL, szam);
+let imageNumber = 11; //starting image index
+function nextImages(){
+    for (let i = 0; i < 5; i++) {
+        fetchImageData(URL, imageNumber);
+        imageNumber++;
     }
+}
+nextImages(); //get default images when the page loads
+
+function previousImages(){
+    if(imageNumber>16){
+        for (let i = 6; i > 0; i--) {
+            fetchImageData(URL, imageNumber);
+            imageNumber--;
+        }
+    }
+    else{
+        console.log('Back to the beginning');
+    }
+}
+
+document.getElementById('nextPage').addEventListener('click', () => {
+    nextImages();
 });
+document.getElementById('previousPage').addEventListener('click', () => {
+    previousImages();
 
-document.getElementById('hatraGomb').addEventListener('click', () => {
-    if(szam>4){
-        szam--;
-        fetchData(URL, szam);
-    }
 });
